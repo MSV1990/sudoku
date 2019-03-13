09534717d5d5767a185dc15c9490bd3ad0986858
MSV1990 <htconemailbox@gmail.com>
2019-03-13 18:38:48 +0300

    solve sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,101 @@
-module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+
+  module.exports = 
+  function solveSudoku(matrix){
+    let row = 0;
+    let col = 0;
+  
+   
+    let cell = findUnassignedLocation(matrix, row, col);
+    row = cell[0];
+    col = cell[1];
+    
+   
+    if (row == -1) {
+    
+      return true;
+    }
+    
+    for (let num = 1; num <= 9; num++) {
+    
+      if ( noConflicts(matrix, row, col, num) ) {   
+          matrix[row][col] = num;
+    
+          if ( solveSudoku(matrix, row, col) ) {                
+              return matrix;
+          }
+    
+     
+          matrix[row][col] = 0;
+      }
+    }
+    
+    
+    return false;
+    }
+    
+    
+    function findUnassignedLocation(matrix, row, col) {
+    let done = false;
+    let res = [-1, -1];
+    
+    while (!done) {
+      if (row == 9) {
+          done = true;
+      }
+      else {
+          if (matrix[row][col] == 0) {
+              res[0] = row;
+              res[1] = col;
+              done = true;
+          }
+          else {
+              if (col < 8) {
+                  col++;
+              }
+              else {
+                  row++;
+                  col = 0;
+              }
+          }
+      }
+    }
+    
+    return res;
+    }
+    
+    function noConflicts(matrix, row, col, num) {
+    return isRowOk(matrix, row, num) && isColOk(matrix, col, num) && isBoxOk(matrix, row, col, num);
+    }
+   
+    function isRowOk(matrix, row, num) {
+    for (let col = 0; col < 9; col++)
+      if (matrix[row][col] == num)
+          return false;
+    
+    return true;
+    }
+  
+    function isColOk(matrix, col, num) {
+    for (let row = 0; row < 9; row++)
+    if (matrix[row][col] == num)
+      return false;
+    
+    return true;    
+    }
+    
+    function isBoxOk(matrix, row, col, num) {
+      row = Math.floor(row / 3) * 3;
+      col = Math.floor(col / 3) * 3;
+    
+    for (let r = 0; r < 3; r++)
+      for (let c = 0; c < 3; c++)
+          if (matrix[row + r][col + c] == num)
+              return false;
+    
+    return true;
+    }
+   
+
+    
+    
+    
